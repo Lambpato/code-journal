aede284b7b86747f838e7a913ac763473671ad28
Lamberto Martinez <lambpato@gmail.com>
2023-02-14 22:29:35 +0000

    Defined function that returns DOM tree

--- js/main.js
+++ js/main.js
@@ -23,5 +23,28 @@ function submitForm(e) {
   e.target.reset();
 }
 
+function renderEntry(entry) {
+  var $liEntry = document.createElement('li');
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  $liEntry.appendChild($row);
+  var $columnHalf = document.createElement('div');
+  $columnHalf.className = 'column-half';
+  $row.appendChild($columnHalf);
+  var $columnHalfLeft = document.createElement('div');
+  $columnHalfLeft.className = 'column-half';
+  $row.appendChild($columnHalfLeft);
+  var $entryImg = document.createElement('img');
+  $columnHalf.appendChild($entryImg);
+  $entryImg.setAttribute('src', $url.value);
+  var $h2 = document.createElement('h2');
+  $h2.textContent = $title.value;
+  var $p = document.createElement('p');
+  $p.textContent = $notes.value;
+  $columnHalfLeft.appendChild($h2);
+  $columnHalfLeft.appendChild($p);
+  return $liEntry;
+}
+
 $form.addEventListener('submit', submitForm);
 $url.addEventListener('input', imageURL);
