c2a50aab510aa8dde9102a0672fddf03a77a3006
Lamberto Martinez <lambpato@gmail.com>
2023-02-14 00:43:24 +0000

    Added submit Listener with functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 var $img = document.querySelector('.entry-img');
 var $url = document.querySelector('#Photo-URL');
+var $notes = document.querySelector('#Notes');
+var $title = document.querySelector('#Title');
+var $form = document.querySelector('form');
+
 function imageURL(e) {
   $img.setAttribute('src', $url.value);
 }
 
+function submitForm(e) {
+  e.preventDefault();
+  var inputs = {
+    title: $title.value,
+    url: $url.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(inputs);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$form.addEventListener('submit', submitForm);
 $url.addEventListener('input', imageURL);
