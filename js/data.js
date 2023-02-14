a6e250807e63e936d0ee007c7271b44222f2596e
Lamberto Martinez <lambpato@gmail.com>
2023-02-14 03:52:13 +0000

    Added beforeunlaod

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var previousDataJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function storeData(e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', storeData);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* global data */
 var $img = document.querySelector('.entry-img');
 var $url = document.querySelector('#Photo-URL');
 var $notes = document.querySelector('#Notes');
@@ -17,9 +18,9 @@ function submitForm(e) {
     entryId: data.nextEntryId
   };
   data.nextEntryId++;
-  data.entries.push(inputs);
+  data.entries.unshift(inputs);
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
+  e.target.reset();
 }
 
 $form.addEventListener('submit', submitForm);
